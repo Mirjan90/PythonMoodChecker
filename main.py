f7e34e8b2fbc1a15c6ddffa9a3d540ac5892db51
Mirjan M <mirjan.miladic@gmail.com>
2021-03-14 15:16:16 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+mood = input("In what mood are you today, please choose between HAPPY, NERVOUS, SAD, RELAXED: ")
+
+if mood == "happy":
+    print("It is great to see you happy!")
+
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+
+elif mood == "sad":
+    print("Dont be sad, life is too short")
+
+elif mood == "relaxed":
+    print("Just enjoy the day!")
+
+else:
+    print("I do not recognize the mood.")
